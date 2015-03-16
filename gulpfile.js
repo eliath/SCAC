5c6a0584deefabcb5fe532838c9da6b702644373
eliath <eliasmartinezcohen@gmail.com>
2015-03-15 22:12:04 -0400

    gulp watch for sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
 	jshintReporter = require('jshint-stylish'),
-	watch = require('gulp-watch');
+	sass = require('gulp-sass');
 
 /*
  * Create variables for our project paths so we can change in one place
  */
 var paths = {
-	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+	'js':	['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json'],
+	'sass':	['./public/styles/**/*.scss']
 };
 
 
-// gulp lint
+// Lint js files
 gulp.task('lint', function(){
-	gulp.src(paths.src)
-		.pipe(jshint())
-		.pipe(jshint.reporter(jshintReporter));
-
-});
-
-// gulp watcher for lint
-gulp.task('watch:lint', function () {
-	gulp.src(paths.src)
-		.pipe(watch())
+	gulp.src(paths.js)
 		.pipe(jshint())
 		.pipe(jshint.reporter(jshintReporter));
 });
+
+// Compile Sass
+gulp.task('sass', function() {
+    gulp.src(paths.sass)
+        .pipe(sass())
+        .pipe(gulp.dest('public/styles/'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    // gulp.watch(paths.js, ['lint']);
+    gulp.watch(paths.sass, ['sass']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'watch']);
